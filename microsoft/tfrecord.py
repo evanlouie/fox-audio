c505f9665131b67b26c7493927586ec8e51acba9
Evan Louie <evan.louie@microsoft.com>
2018-10-27 18:48:03 -0700

    Inference Report and Cleanup
    - Wrote inference report for performance with Adaptive Learning with AudioSet
    - Cleanuped and reformatted repository
    - Added multiprocessing to `white_noise_padding.py`

---
+++ microsoft/tfrecord.py
@@ -0,0 +1,35 @@
+import argparse
+import tensorflow as tf
+import os
+
+
+def read(record_path, is_sequence_example=True):
+    # Tensorflow uses weird reflection on protobufs. The FromString method does exist; disable E1101 warning
+    return [
+        (
+            tf.train.SequenceExample.FromString(record)  # pylint: disable=E1101
+            if is_sequence_example
+            else tf.train.Example.FromString(record)  # pylint: disable=E1101
+        )
+        for record in tf.python_io.tf_record_iterator(record_path)
+    ]
+
+
+def print_example(record_path, is_sequence_example=True):
+    """Prints the contents of a TF Record file containing either a SequenceExample or Example."""
+    examples = read(record_path, is_sequence_example)
+    for example in examples:
+        print(example)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sequence_example", help="target TFRecord SequenceExample file")
+    parser.add_argument("--example", help="target TFRecord Example file")
+    args = parser.parse_args()
+
+    if args.example:
+        print_example(args.example, is_sequence_example=False)
+    else:
+        print_example(args.sequence_example)
+
