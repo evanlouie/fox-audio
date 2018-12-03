f0ed68717e092fb47d0d1570cb3c97a220ea1960
Edaena Salinas Jasso <edaenasj@microsoft.com>
2018-12-03 11:57:27 -0800

    Setup for flask server

--- youtube8m/app.py
+++ youtube8m/app.py
@@ -1,31 +1,18 @@
-import sys
-from sanic import Sanic
-from sanic.response import json, text
-from sanic.config import Config
-from server import Server
-import server
+from flask import Flask, request, flash, jsonify
+import os
 
-def main():
-  app = Sanic(__name__)
-  Config.KEEP_ALIVE = False
+app = Flask(__name__)
 
-  server = Server()
+@app.route("/")
+def home():
+    return "Hello, Flask!"
 
-  @app.route('/')
-  def test(request):
-      return text(server.server_running())
+@app.route("/inference", methods=['POST'])
+def inference():
+    f = request.files['fieldNameHere']
 
-  @app.route('/tfrecord', methods=["POST"])
-  def vggish(request):
-      return(server.get_tfrecord())
-
-  @app.route('/inference', methods=["POST"])
-  def post_json(request):
-      return json(server.get_inference())
-
-  app.run(host= '0.0.0.0', port=80)
-  print('exiting...')
-  sys.exit(0)
-
-if __name__ == '__main__':
-  main()
+    f.save(os.path.join('upload', 'testing.wav'))
+    if f.filename == '':
+            flash('No selected file')
+    
+    return "Finished."
