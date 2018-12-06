7fc223c1258655e527acd4ee06259d470d763ada
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2018-12-06 13:14:25 -0800

    debugging

--- serverexample/app.py
+++ serverexample/app.py
@@ -3,10 +3,12 @@ import os
 import server
 import io
 from scipy.io import wavfile
+import uuid
 
 app = Flask(__name__)
 app.debug = True
-#app.run()
+
+guid = str(uuid.uuid4())
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
@@ -30,15 +32,20 @@ def home():
 def inference():
     #To-do: error handling for min requirement files
 
-    uploaded_file_name = next(iter(request.files))
-    uploaded_file = request.files[uploaded_file_name]
-    wav_filename = uploaded_file.filename
+    uploaded_files_name_property = next(iter(request.files))
+    files = request.files.getlist(uploaded_files_name_property)
+    #uploaded_file = request.files[uploaded_file_name]
+    #wav_filename = uploaded_file.filename
 
-    # Get the content from the uploaded wav
-    uploaded_file.seek(0)
-    wav_content = uploaded_file.read()
+    for wav_file in files:
+        wav_filename = wav_file.filename
+        # Get the content from the uploaded wav
+        wav_file.seek(0)
+        wav_content = wav_file.read()
 
-    server.get_tfrecord_from_file(wav_filename, io.BytesIO(wav_content))
-    json = server.get_inf_json()
-    
-    return jsonify(json)
+        server.get_tfrecord_from_file(wav_filename, io.BytesIO(wav_content), guid + "-" + wav_filename + ".tfrecord")
+        
+        #return jsonify(json)
+    json = server.get_inf_json(guid + "*.tfrecord")
+    print(str(json))
+    return("Finished")
